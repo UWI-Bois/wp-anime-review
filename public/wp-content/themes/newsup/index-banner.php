21adfdbae700aaf7c6dfd5af3432f05cfaf7e1c6
Ajay <ajay_sieunarine@hotmail.com>
2020-04-05 09:01:22 -0400

    removed title from breadcrumb at top of post singles

--- public/wp-content/themes/newsup/index-banner.php
+++ public/wp-content/themes/newsup/index-banner.php
@@ -5,23 +5,23 @@ if ( has_header_image() ) {
   $newsup_background_image = get_header_image();
 }
 ?>
-<div class="mg-breadcrumb-section" style='background: url("<?php echo esc_url( $newsup_background_image ); ?>" ) repeat scroll center 0 #143745;'>
-<?php $newsup_remove_header_image_overlay = get_theme_mods('remove_header_image_overlay',true);
-if($newsup_remove_header_image_overlay == true){ ?>
-  <div class="overlay">
-<?php } ?>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12 col-sm-12">
-			    <div class="mg-breadcrumb-title">
-            <h1><?php the_title(); ?></h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  <?php $newsup_remove_header_image_overlay = get_theme_mods('remove_header_image_overlay',true);
-if($newsup_remove_header_image_overlay == true){ ?>
-  </div>
-<?php } ?>
-</div>
-<div class="clearfix"></div>
+<!--<div class="mg-breadcrumb-section" style='background: url( "--><?php //echo esc_url( $newsup_background_image ); ?>
+<?php //$newsup_remove_header_image_overlay = get_theme_mods('remove_header_image_overlay',true);
+//if($newsup_remove_header_image_overlay == true){ ?>
+<!--  <div class="overlay">-->
+<?php //} ?>
+<!--    <div class="container-fluid">-->
+<!--      <div class="row">-->
+<!--        <div class="col-md-12 col-sm-12">-->
+<!--			    <div class="mg-breadcrumb-title">-->
+<!--            <h1>--><?php //the_title(); ?><!--</h1>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  --><?php //$newsup_remove_header_image_overlay = get_theme_mods('remove_header_image_overlay',true);
+//if($newsup_remove_header_image_overlay == true){ ?>
+<!--  </div>-->
+<?php //} ?>
+<!--</div>-->
+<!--<div class="clearfix"></div>-->
