7762e91156a1ab717dd6efa07350a3eb24788636
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-06 12:11:13 -0400

    Add Blog Post Index page template.

---
+++ public/wp-content/themes/newsup-child/home.php
@@ -0,0 +1,20 @@
+<?php
+  get_header(  );
+?>
+
+<?php
+  // This page template is intended by Wordpress file hierarchy to
+  // load the Blog Post Index Page; the page will load all default
+  // Wordpress Post types in the database.
+  //
+  // The Blog Post Index Page typically uses this file first, and
+  // will default to using index.php as a "catch all" file template.
+  //
+  // In order to achieve what we want for this website, this page
+  // will be modified to use a WP_Query against all "Anime" and
+  // "Review" custom post types.
+?>
+
+<?php
+  get_footer(  );
+?>
