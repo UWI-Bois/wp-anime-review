96aaf6cdef0eedc56e879804562b11231c9db8d8
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-16 19:46:51 -0400

    Post Author -> Anime Author (front-page.php)

--- public/wp-content/themes/newsup-child/front-page.php
+++ public/wp-content/themes/newsup-child/front-page.php
@@ -72,7 +72,7 @@ get_header(); ?>
                                                                              <span class="mg-blog-date"><i class="fa fa-clock-o"></i>
                                                                              <a href="<?php echo esc_url(get_month_link(get_post_time('Y'),get_post_time('m'))); ?>">
                                                                              <?php echo esc_html(get_the_date('M j, Y')); ?></a></span>
-                                                                             <a href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) ));?>"><i class="fa fa-user-circle-o"></i> <?php the_author(); ?></a>
+                                                                             <span><i class="fa fa-user-circle-o"></i> <?php echo (get_field('anime_author') ? get_field('anime_author'): 'Unknown'); ?></span>
                                                                          </div>
                                                                      </div>
 
